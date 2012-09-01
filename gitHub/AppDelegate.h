d9c90d86f6bdb192d0ae8ea31a9b1f19c1c3a107
gragon <suwit0303@gmail.com>
2012-09-01 11:50:57 +0700

    fist commit

---
+++ gitHub/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitHub
+//
+//  Created by Suwit on 01/09/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitHub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  gitHub
+//
+//  Created by Suwit on 01/09/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
